120f415d5f0a51b3949d393f0b6ad7e426cd4cf7
Sameque Santana <samequesantana@gmail.com>
2020-12-31 19:40:55 -0300

    First commit.

---
+++ Transactions.ClientFactory/Interfaces/IHttpClinetFactoryBank.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Transactions.Models;
+
+namespace Transactions.ClientFactory.Interfaces
+{
+    public interface IHttpClinetFactoryBank
+    {
+        [Get("/banks")]
+        Task<List<Bank>> GetAll();
+    }
+}
---
+++ Transactions.Teste/BankTeste.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Transactions.Teste
+{
+    public class BankTeste
+    {
+        private IHttpClientFactory _httpClientFactory;
+
+        public HttpClient Client { get; private set; }
+        [Fact]
+        public async Task List()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = server.CreateClient();
+
+            var response = await Client.GetAsync("/banks");
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal(HttpStatusCode.OK,response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = server.CreateClient();
+
+            var response = await Client.GetAsync("/bank/1");
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal(HttpStatusCode.OK,response.StatusCode);
+        }
+    }
+}
